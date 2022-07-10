4fd5b14937ee4402851e3e4e8c11d2ab4b564d31
RayenBSD <92104474+RayenBSD@users.noreply.github.com>
2022-07-10 16:24:47 +0100

    Add files via upload

---
+++ landing page/script.js
@@ -0,0 +1,11 @@
+
+    const img = document.querySelector(".mainImg");
+    const main = document.querySelector("main");
+
+    function phones (phone) {
+        img.src = phone;
+    }
+
+    function color (clr) {
+        main.style.background = clr;
+    }
